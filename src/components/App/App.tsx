460877b2b4c0c87be63f8e58a74706c3770681e7
Filip <filipsarovic428@gmail.com>
2021-08-17 22:11:16 +0200

    "importing pokemons"

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,32 @@
 import React from 'react';
+import { pokemonData } from '../../data/PokemonData';
 import Pokedex from '../Pokedex/Pokedex';
 import './App.css';
 
-class App extends React.Component {
+interface AppState {
+    searchField: string;
+    allPokemons: any;
+    searchedPokemons: any;
+    selectedPokemon: any;
+}
+
+
+class App extends React.Component<any, AppState> {
+    state = {
+        searchField: "",
+        allPokemons: [],
+        searchedPokemons: [],
+        selectedPokemon: undefined
+    };
+
+    componentDidMount () {
+        const pokeData = pokemonData;
+        console.log(pokeData);
+        
+        
+    }
+
+
     render() {
         return <div className="App">
             <h1>Pokedex</h1>
